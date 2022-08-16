6e2dda7b0cdf19b16dfde2e120fed3010ac9a395
Iyana Marquez <yayakix@gmail.com>
2022-08-16 11:29:52 -0400

    prevent crud functions on non auth user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_14_030932) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_16_151717) do
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_14_030932) do
     t.string "twitter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
